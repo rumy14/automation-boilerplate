15170a6d910ce339823ac79798c91c001b543ee4
unknown <miturumy@gmail.com>
2024-04-23 11:54:44 +0600

    test maven

---
+++ src/test/java/automation/smoke/SeleniumTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to a website
+        driver.get("https://www.example.com");
+
+        // Print the title of the webpage
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
+}
